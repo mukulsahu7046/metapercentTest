c000b6444dfd346734f812a9d7ef4d020cb83eb7
mukulsahu7046 <mukulsahu11@gamil.com>
2023-01-25 17:40:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch("./class.json").then((res)=>res.json()).then((data)=>console.log(data));
+
+const temp= `<img src="${dt.ImageUrl}">
+				<h1>${dt.ID}</h1>
+				<p>${dt.ShortDesc}</p>
+				<button>view</button>`
+data.map(function(document.getElementById("grid").innerHTML=`${temp}`));
